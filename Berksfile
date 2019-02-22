ddca2a57425c822081e4a21ebbd44f5be05ac64c
Jesse Piccolo <jpiccolo@frontlineed.com>
2019-02-22 09:37:40 -0500

    Getting into chef automate

--- Berksfile
+++ Berksfile
@@ -1,3 +1,4 @@
+source 'http://artifactory.ss.frontlineeducation.com/artifactory/api/chef/fl-chef-virtual'
 source 'https://supermarket.chef.io'
 
 metadata
--- metadata.rb
+++ metadata.rb
@@ -1,29 +1,23 @@
 name             'nxlog'
-version          '0.8.7'
-
+maintainer       'Systems Team'
+maintainer_email 'DIST_Technology_Dev_Systems@frontlineed.com'
+license          'Apache v2.0'
 description      'Installs and configures NXLog_ce on Linux or Windows'
+version          '0.8.8'
+recipe           'nxlog::default', 'Installs and configures NXLog_ce on Linux or Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url  'https://github.com/FrontlineEducation/nxlog' if respond_to?(:source_url)
+issues_url  'https://github.com/FrontlineEducation/nxlog/issues' if respond_to?(:issues_url)
+chef_version '>= 13.8' if respond_to?(:chef_version)
 
-issues_url       'http://github.com/widgit-cookbooks/nxlog/issues'
-source_url       'http://github.com/widgit-cookbooks/nxlog/'
-
-provides         'nxlog::default'
-provides         'nxlog::papertrail'
-provides         'nxlog::syslog'
-provides         'service[nxlog]'
-
+# Operating Systems Supported
 supports         'ubuntu', '>= 12.04'
 supports         'debian', '>= 6.0'
 supports         'redhat', '>= 6.0'
 supports         'centos', '>= 6.0'
 supports         'windows'
 
+
 depends          'zap'
 
-recipe           'nxlog::default', 'Install and configure nxlog'
-recipe           'nxlog::papertrail', 'Enable nxlog Papertrail support'
-recipe           'nxlog::syslog', 'Sets up nxlog as the system logger'
 
-maintainer       'Simon Detheridge'
-maintainer_email 'simon@widgit.com'
-license          'Apache v2.0'
